09e9953f824d5344dd542ccf095596aa14eb282d
Salimova Ramina <salimovaramina.e@gmail.com>
2024-10-26 18:03:41 +0400

    First commit

---
+++ src/main/java/az/edu/turing/Main.java
@@ -0,0 +1,22 @@
+package az.edu.turing;
+public class Main {
+    public static void main(String[] args) {
+        int c = 333;
+        long d = 444L;
+        System.out.println(c+d);
+        int number1 = 15;
+        int number2 = 7;
+        int sum = number1 + number2;
+        int substract = number1 - number2;
+        float divide = (float) number1 / number2;
+        int multiply = number1 * number2;
+        int divide2 = number1 % number2;
+        System.out.println(sum);
+        System.out.println(substract);
+        System.out.println(divide);
+        System.out.println(divide2);
+        System.out.println(multiply);
+
+
+    }
+}
